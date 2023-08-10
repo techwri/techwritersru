2df18cbd009ea46de13ad16851e7309d1de9320f
magic <magic@internet.ru>
2023-08-10 17:19:33 +0300

    Добавлен путь к шаблону стилей

--- conf.py
+++ conf.py
@@ -35,7 +35,7 @@ plantuml_output_format = 'png'
 
 templates_path = ['_templates']
 exclude_patterns = []
-languages = ['en', 'ru']  # Список поддерживаемых языков
+languages = ['ru', 'en']  # Список поддерживаемых языков
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -45,6 +45,7 @@ languages = ['en', 'ru']  # Список поддерживаемых языко
 html_theme = 'sphinx_book_theme'  # используемая тема сайта
 html_title = "TechWriters.ru"  # название сайта
 html_static_path = ['source/_static']  # путь к статичным файлам
+html_css_files = ["custom.css"]  # путь к костомизированным файлам стиля
 
 
 html_theme_options = {
